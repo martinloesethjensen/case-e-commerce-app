6d10dfe016c8c7171faaf9f45b11b93af9feec3b
Martin Jensen <martinloesethjensen@gmail.com>
2026-02-14 23:41:26 +0100

    provide CategoriesRepository

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,10 +5,13 @@ import { provideHttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
+import { CategoriesRepository } from './app/domain/repositories/categories.repository';
+import { CategoriesRepositoryImpl } from './app/data/repositories/categories.repository.impl';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: CategoriesRepository, useClass: CategoriesRepositoryImpl },
     provideIonicAngular(),
     provideRouter(routes),
     provideHttpClient(),
